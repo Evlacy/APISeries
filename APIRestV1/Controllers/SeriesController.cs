4fcc3b515044799031efa42bc319db90b6e2d9b3
Evlacy <ahmet.altinel@etu.univ-smb.fr>
2023-02-02 09:22:07 +0100

    DBContext

---
+++ APIRestV1/Controllers/SeriesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APIRestV1.Models.EntityFramework;
+
+namespace APIRestV1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeriesController : ControllerBase
+    {
+        private readonly SeriesDbContext _context;
+
+        public SeriesController(SeriesDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Series
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Serie>), 200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<IEnumerable<Serie>>> GetSeries()
+        {
+            return await _context.Series.ToListAsync();
+        }
+
+        // GET: api/Series/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Serie))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Serie>> GetSerie(int id)
+        {
+            var serie = await _context.Series.FindAsync(id);
+
+            if (serie == null)
+            {
+                return NotFound();
+            }
+
+            return serie;
+        }
+
+        // PUT: api/Series/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> PutSerie(int id, Serie serie)
+        {
+            if (id != serie.Serieid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(serie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SerieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Series
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Serie>> PostSerie(Serie serie)
+        {
+            _context.Series.Add(serie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSerie", new { id = serie.Serieid }, serie);
+        }
+
+        // DELETE: api/Series/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteSerie(int id)
+        {
+            var serie = await _context.Series.FindAsync(id);
+            if (serie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Series.Remove(serie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        private bool SerieExists(int id)
+        {
+            return _context.Series.Any(e => e.Serieid == id);
+        }
+    }
+}
--- APIRestV1/Program.cs
+++ APIRestV1/Program.cs
@@ -1,6 +1,14 @@
+using APIRestV1.Models.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<SeriesDbContext>(options =>
+  options.UseNpgsql(builder.Configuration.GetConnectionString("SeriesDbContext")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

23604a0554651b7e6edb8f1a8f235bfeb475d902
Evlacy <ahmet.altinel@etu.univ-smb.fr>
2023-02-03 11:08:06 +0100

    Début tests

--- APIRestV1/Models/EntityFramework/Serie.cs
+++ APIRestV1/Models/EntityFramework/Serie.cs
@@ -9,6 +9,27 @@ namespace APIRestV1.Models.EntityFramework;
 [Table("serie")]
 public partial class Serie
 {
+    public Serie(int serieid, string titre, string resume, int nbsaisons, int nbepisodes, int anneecreation, string network)
+    {
+        Serieid = serieid;
+        Titre = titre;
+        Resume = resume;
+        Nbsaisons = nbsaisons;
+        Nbepisodes = nbepisodes;
+        Anneecreation = anneecreation;
+        Network = network;
+    }
+    public Serie(string titre, string resume, int nbsaisons, int nbepisodes, int anneecreation, string network)
+    {
+        Titre = titre;
+        Resume = resume;
+        Nbsaisons = nbsaisons;
+        Nbepisodes = nbepisodes;
+        Anneecreation = anneecreation;
+        Network = network;
+    }
+
+
     [Key]
     [Column("serieid")]
     public int Serieid { get; set; }
@@ -18,18 +39,18 @@ public partial class Serie
     public string Titre { get; set; } = null!;
 
     [Column("resume")]
-    public string? Resume { get; set; }
+    public string Resume { get; set; }
 
     [Column("nbsaisons")]
-    public int? Nbsaisons { get; set; }
+    public int Nbsaisons { get; set; }
 
     [Column("nbepisodes")]
-    public int? Nbepisodes { get; set; }
+    public int Nbepisodes { get; set; }
 
     [Column("anneecreation")]
-    public int? Anneecreation { get; set; }
+    public int Anneecreation { get; set; }
 
     [Column("network")]
     [StringLength(50)]
-    public string? Network { get; set; }
+    public string Network { get; set; }
 }
